958aafc9ff65ed20ecac3052bec44630fcd5170e
Arun yadav <yadavaarun01@gmail.com>
2024-08-12 07:06:57 +0530

    First Commit

--- src/main/java/com/stream/app/spring_stream_backend/entity/Video.java
+++ src/main/java/com/stream/app/spring_stream_backend/entity/Video.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity
@@ -18,6 +19,7 @@ public class Video {
     private String videoId;
     private String title;
     private String description;
+    @NotBlank(message = "Content type is required.")
     private String contentType;
     private String filePath;
 
